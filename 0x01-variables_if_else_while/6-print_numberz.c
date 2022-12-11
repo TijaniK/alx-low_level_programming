713da768dd32b78ffd7db5e4af190fec5071d43f
Tijani <tijani.kallel860@gmail.com>
2022-12-11 12:23:53 -0800

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all digits from 0 to 9.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar ((i%10) + '0');
+	putchar ('\n');
+	return (0);
+}
