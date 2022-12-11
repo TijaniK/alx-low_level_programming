670ef616327eac167e7224f586bb0eacc13a578e
Tijani <tijani.kallel860@gmail.com>
2022-12-11 12:52:45 -0800

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char i;
+	
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		putchar(i);
+		if (i == '9')
+			continue;
+        putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
